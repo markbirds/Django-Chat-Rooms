cab8c37dcaec8fb8a09b660675ab1ad08c37af9c
Owen <fowenpatrick@gmail.com>
2020-11-04 14:44:19 +0800

    collect static

--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -137,6 +137,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ASGI_APPLICATION = 'django_chatroom.routing.application'
 
